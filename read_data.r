bf61cd739a986e7502020c6ae5a3c1d837188d47
Weichun <weichun5795@gmail.com>
2016-03-20 22:31:40 +0800

    adjust colnames

--- read_data.r
+++ read_data.r
@@ -7,21 +7,22 @@ setwd("/Users/weichun/Desktop/creditcard")
 data_list <- vector("list", 11)
 names(data_list) <- c(94:104)
 for (i in 94:102) {
-	cat(i,"年", "\n")
-
+	# cat(i,"年", "\n")
 	monthly_data <- vector("list", 12)
 	for (j in 1:12) {
-		cat(j, "月", "\n")
+		# cat(j, "月", "\n")
 		filename <- paste("data/", i, "/", j, ".xls", sep="")
 		temp <- read_excel(filename)
 		m <- as.matrix(temp)
 		m[ is.na(m) ] <- ""
+		m <- gsub("[[:space:]]", "", m)
 
 		# set colnames
 		colname_index <- which( m[, 1] == "金融機構名稱", arr.ind=T )
 		colname_index2 <- tail(which( m[1:10, 1] == "", arr.ind=T ), 1)
 		data_col_names <- apply(m[colname_index:colname_index2, ], 2, function(x) paste(x, collapse=""))
 		data_col_names <- gsub("[[:space:]]", "", data_col_names)
+		# cat(data_col_names, "\n")
 		colnames(m) <- data_col_names
 
 		total_idx <- which(m == "總計", arr.ind=T)[1, 1]
@@ -33,20 +34,26 @@ for (i in 94:102) {
 
 		col_count <- ncol(m)
 		m <- m[,(-1)]
+		# cat(colnames(m), "bbbbbb\n")
+		data_col_names <- colnames(m)
+		# cat(data_col_names, "aaaaaaa\n")
 		m[ is.na(m) ] <- 0
 		m <- t(apply(m, 1, function(x) as.numeric(x)))
 
 		monthly_data[[j]] <- m
+		colnames(monthly_data[[j]]) <- data_col_names
+
+		# cat(colnames(monthly_data[[j]]), "aaa\n")
 	}
 	data_list[[i]] <- monthly_data
 }
 
 
 for (i in 103:104) {
-	cat(i, "年\n")
+	# cat(i, "年\n")
 	monthly_data <- vector("list", 12)
 	for (j in 1:12) {
-		cat(j, "月", "\n")
+		# cat(j, "月", "\n")
 		filename <- paste("data/", i, "/", j, ".xlsx", sep="")
 		temp <- read_excel(filename)
 		m <- as.matrix(temp)
@@ -59,7 +66,7 @@ for (i in 103:104) {
 		# cat(colname_index2, "\n")
 		data_col_names <- apply(m[colname_index:colname_index2, ], 2, function(x) paste(x, collapse=""))
 		data_col_names <- gsub("[[:space:]]", "", data_col_names)
-		cat(data_col_names, "\n")
+		# cat(data_col_names, "\n")
 		colnames(m) <- data_col_names
 
 		total_idx <- which(m == "總計", arr.ind=T)[1, 1]
@@ -71,10 +78,12 @@ for (i in 103:104) {
 
 		col_count <- ncol(m)
 		m <- m[,(-1)]
+		data_col_names <- colnames(m)
 		m[ is.na(m) ] <- 0
 		m <- t(apply(m, 1, function(x) as.numeric(x)))
 
 		monthly_data[[j]] <- m
+		colnames(monthly_data[[j]]) <- data_col_names
 	}
 	data_list[[i]] <- monthly_data
 }
